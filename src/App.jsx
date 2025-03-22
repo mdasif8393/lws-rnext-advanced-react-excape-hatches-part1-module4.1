af8895499e34aa1b0b1e97a35521d7dc7b3dc0a1
mdasif8393 <md.asif.8393@gmail.com>
2025-03-22 23:43:15 +0600

    4.6 Flushing state updates synchronously with flushSync

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Form from "./Form";
 import "./App.css";
 import Counter from "./components/Counter";
+import TodoList from "./components/ToDoList";
 
 export default function App() {
   return (
     <>
-      <Counter />
+      <TodoList />
     </>
   );
 }
